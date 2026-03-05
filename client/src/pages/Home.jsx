209b473dddc93ac09ef49451e63551aa0d4b6d28
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-05 18:40:34 +0530

    code trailer section and mound it in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import FeaturedSections from '../components/FeaturedSection';
+import TrailersSection from '../components/TrailersSection';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <FeaturedSections />
+      <TrailersSection />
     </>
   );
 };
