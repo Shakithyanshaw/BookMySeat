e15f4d95e869264add1df9516324ea18b3ad2b15
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-06 01:15:40 +0530

    add tailwind classe for footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const Footer = () => {
   return (
-    <footer className="px-6 pt-8 md:px-16 lg:px-36 w-full text-gray-300">
-      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-10">
+    <footer className="px-6  md:px-16 lg:px-36 mt-40 w-full text-gray-300">
+      <div className="flex flex-col md:flex-row justify-between w-full gap-10 border-b border-gray-500 pb-14">
         <div className="md:max-w-96">
-          <img
-            alt=""
-            class="h-11"
-            src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/prebuiltuiLogoSquareShape.svg"
-          />
+          <img alt="" class="h-11" src={assets.logo} />
           <p className="mt-6 text-sm">
             Lorem Ipsum has been the industry's standard dummy text ever since
             the 1500s, when an unknown printer took a galley of type and
@@ -17,15 +14,11 @@ const Footer = () => {
           </p>
           <div className="flex items-center gap-2 mt-4">
             <img
-              src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/googlePlayBtnBlack.svg"
+              src={assets.googlePlay}
               alt="google play"
-              className="h-10 w-auto border border-white rounded"
+              className="h-9 w-auto "
             />
-            <img
-              src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/refs/heads/main/assets/appDownload/appleStoreBtnBlack.svg"
-              alt="app store"
-              className="h-10 w-auto border border-white rounded"
-            />
+            <img src={assets.appStore} alt="app store" className="h-9 w-auto" />
           </div>
         </div>
         <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
@@ -57,7 +50,7 @@ const Footer = () => {
       </div>
       <p className="pt-4 text-center text-sm pb-5">
         Copyright {new Date().getFullYear()} ©{' '}
-        <a href="https://prebuiltui.com">PrebuiltUI</a>. All Right Reserved.
+        <a href="https://prebuiltui.com">QuicShow</a>. All Right Reserved.
       </p>
     </footer>
   );
