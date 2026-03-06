1149fdeafc96d1aefe7f5e51db186fd6d55b95da
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-07 01:49:30 +0530

    implement favourite cast section with horizontal scroll

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -54,7 +54,7 @@ const MovieDetails = () => {
               <PlayCircle className="w-5 h-5" /> Watch Trailer
             </button>
             <a
-              href=""
+              href="#dateSelect"
               className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer active:scale-95"
             >
               Buy Tickets
@@ -63,6 +63,22 @@ const MovieDetails = () => {
               <Heart className={`w-5 h-5`} />
             </button>
           </div>
+        </div>
+      </div>
+
+      <p className="text-lg font-medium mt-20">Your Favourite Cast</p>
+      <div className="overflow-x-auto no-scrollbar mt-8 pb-4">
+        <div className="flex items-center gap-4 w-max px-4">
+          {show.movie.casts.slice(0, 12).map((cast, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <img
+                src={cast.profile_path}
+                alt=""
+                className="rounded-full h-20 md:h-20 aspect-square object-cover"
+              />
+              <p className="font-medium text-xs mt-3">{cast.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
