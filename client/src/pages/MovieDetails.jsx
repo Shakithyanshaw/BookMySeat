1aa5471feaa9f9fa5153ab2656d1dfa4dd18c135
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-07 00:29:34 +0530

    add MovieDetails page with show data fetching using useParams

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useParams from 'react-router-dom';
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 
 const MovieDetails = () => {
-  return <div></div>;
+  const { id } = useParams();
+  const [show, setShow] = useState(null);
+
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    setShow({
+      movie: show,
+      dateTime: dummyDateTimeData,
+    });
+  };
+
+  useEffect(() => {
+    getShow();
+  }, [id]);
+
+  return show ? (
+    <div className="px-6 md:px-16 lg:px-40 pt-30 md:pt-50">
+      <div></div>
+    </div>
+  ) : (
+    <div>Loading</div>
+  );
 };
 
 export default MovieDetails;
