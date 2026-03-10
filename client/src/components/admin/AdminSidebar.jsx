ec8effa6ebdcab5f5d5efc2d3ddb2ddf748ce33a
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-11 00:34:45 +0530

    implement Dashboard component with stats cards and dummy data

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -44,7 +44,7 @@ const AdminSidebar = () => {
             to={link.path}
             end
             className={({ isActive }) =>
-              `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 min-md:pl-10 first:mt-6 text-gray-400 ${isActive && 'bg-primary/15 text-primary group'}`
+              `relative flex items-center max-md:justify-center gap-2 w-full py-2.5 md:pl-10 first:mt-6 text-gray-400 ${isActive && 'bg-primary/15 text-primary group'}`
             }
           >
             {({ isActive }) => (
--- client/src/pages/admin/Dashbord.jsx
+++ client/src/pages/admin/Dashbord.jsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import {
+  ChartLineIcon,
+  CircleDollarSignIcon,
+  PlayCircleIcon,
+  UserIcon,
+} from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import { dummyDashboardData } from '../../assets/assets';
 
 const Dashbord = () => {
-  return <div></div>;
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [dashboardData, setDashboardData] = useState({
+    totalBookings: 0,
+    totalRevenue: 0,
+    activeShows: [],
+    totalUser: 0,
+  });
+  const [loading, setLoading] = useState(true);
+
+  const dashboardCards = [
+    {
+      title: 'Total Bookings',
+      value: dashboardData.totalBookings || '0',
+      icon: ChartLineIcon,
+    },
+    {
+      title: 'Total Revenue',
+      value: dashboardData.totalBookings || '0',
+      icon: CircleDollarSignIcon,
+    },
+    {
+      title: 'Total Shows',
+      value: dashboardData.activeShows.length || '0',
+      icon: PlayCircleIcon,
+    },
+    {
+      title: 'Total Users',
+      value: dashboardData.totalUser || '0',
+      icon: UserIcon,
+    },
+  ];
+
+  const fetchDashboardData = async () => {
+    setDashboardData(dummyDashboardData);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
 };
 
 export default Dashbord;
