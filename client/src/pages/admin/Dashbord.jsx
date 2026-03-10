0cef5493d29d45acdcf3562df142a504f77d326b
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-11 00:51:11 +0530

    implement admin dashboard with booking, revenue, shows and users summary cards

--- client/src/pages/admin/Dashbord.jsx
+++ client/src/pages/admin/Dashbord.jsx
@@ -8,6 +8,7 @@ import React, { useEffect, useState } from 'react';
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
+import BlurCircle from '../../components/BlurCircle';
 
 const Dashbord = () => {
   const currency = import.meta.env.VITE_CURRENCY;
@@ -28,7 +29,7 @@ const Dashbord = () => {
     },
     {
       title: 'Total Revenue',
-      value: dashboardData.totalBookings || '0',
+      value: currency + dashboardData.totalRevenue || '0',
       icon: CircleDollarSignIcon,
     },
     {
@@ -55,6 +56,24 @@ const Dashbord = () => {
   return !loading ? (
     <>
       <Title text1="Admin" text2="Dashboard" />
+
+      <div className="relative flex flex-wrap gap-4 mt-6">
+        <BlurCircle top="-100px" left="0" />
+        <div className="flex flex-wrap gap-4 w-full">
+          {dashboardCards.map((card, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between px-4 py-3 bg-primary/10 border border-primary/20 rounded-md max-w-50 w-full"
+            >
+              <div>
+                <h1 className="text-sm">{card.title}</h1>
+                <p className="text-xl font-medium mt-1">{card.value}</p>
+              </div>
+              <card.icon className="w-6 h-6" />
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   ) : (
     <Loading />
