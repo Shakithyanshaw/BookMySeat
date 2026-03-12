b79cc8030fc8fde85776219884020c7f5f758e6e
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-12 13:12:59 +0530

    add AddShows component with movie selection and state setup

--- client/src/pages/admin/AddShows.jsx
+++ client/src/pages/admin/AddShows.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Title from '../../components/admin/Title';
+import { dummyShowsData } from '../../assets/assets';
+import Loading from '../../components/Loading';
 
 const AddShows = () => {
-  return <div></div>;
+  const currency = import.meta.env.VITE_CURRENCY;
+
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [dateTimeSelection, setDateTimeSelection] = useState({});
+  const [dateTimeInput, setDateTimeInput] = useState('');
+  const [showPrice, setShowPrice] = useState('');
+
+  const fetchNowPlayingMovies = async () => {
+    setNowPlayingMovies(dummyShowsData);
+  };
+
+  useEffect(() => {
+    fetchNowPlayingMovies();
+  }, []);
+
+  return nowPlayingMovies.length > 0 ? (
+    <>
+      <Title text1="Add" text2="Shows" />
+    </>
+  ) : (
+    <Loading />
+  );
 };
 
 export default AddShows;
