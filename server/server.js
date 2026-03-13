3d0e728f1090ba686cbaa54c66271b8d364f3cd0
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-14 01:04:09 +0530

    install inngest for background job handling

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express';
 
 const app = express();
 const port = 5000;
 
-//Db
+//DB
 await connectDB();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
 // API Routes
 app.get('/', (req, res) => res.send('Server is Live!'));
