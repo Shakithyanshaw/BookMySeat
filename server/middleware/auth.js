5b211d3efa331937405c59b1815d9dd6805e55a0
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-18 03:05:27 +0530

    fixing admin login problem

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,21 +4,14 @@ export const protectAdmin = async (req, res, next) => {
   try {
     const { userId } = req.auth;
 
-    if (!userId) {
-      return res.status(401).json({ success: false, message: 'Unauthorized' });
-    }
-
     const user = await clerkClient.users.getUser(userId);
 
-    if (!user.privateMetadata?.role || user.privateMetadata.role !== 'admin') {
-      return res
-        .status(403)
-        .json({ success: false, message: 'Forbidden: Admins only' });
+    if (user.privateMetadata.role !== 'admin') {
+      return res.json({ success: false, message: 'not authorized' });
     }
 
     next();
   } catch (error) {
-    console.error('ProtectAdmin error:', error);
-    return res.status(500).json({ success: false, message: 'Server error' });
+    return res.json({ success: false, message: 'not authorized' });
   }
 };
