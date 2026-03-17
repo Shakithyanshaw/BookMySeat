06390636a9d66d3debc8d3f6ba22d3c2324715d1
Shakithyanshaw <jeyakumarshakithyan03@gmail.com>
2026-03-17 12:00:28 +0530

    create function to admin check and dashboard data API

---
+++ server/controllers/adminController.js
@@ -0,0 +1,32 @@
+import Booking from '../models/Booking.js';
+import Show from '../models/Show.js';
+import User from '../models/User.js';
+
+// API to check if user is admin
+export const isAdmin = async (req, res) => {
+  res.json({ success: true, isAdmin: true });
+};
+
+// API to get dashboard data
+export const getDashboardData = async (req, res) => {
+  try {
+    const bookings = await Booking.find({ isPaid: true });
+    const activeShows = await Show.find({
+      showDateTime: { $gte: new Date() },
+    }).populate('movie');
+
+    const tototalUser = await User.countDocuments();
+
+    const dashboardData = {
+      totalBookings: bookings.length,
+      totalRevenue: bookings.reduce((acc, booking) => acc + booking.amount, 0),
+      activeShows,
+      tototalUser,
+    };
+
+    res.json({ success: true, dashboardData });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
